ced348108c63d570e255f5dafc2dfde259cc245a
momenur <momenurislam6@gmail.com>
2023-02-14 12:48:06 +0600

    finish with Condition

--- js/bank.js
+++ js/bank.js
@@ -1,12 +1,15 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const depositAmountString = document.getElementById('deposit-input-filed');
     const depositAmount = parseFloat(depositAmountString.value);
-    console.log('input amount', depositAmount, typeof depositAmount);
+    
+    
     const depositElemnt = document.getElementById('deposit-toal-amount');
     const depositTotal = depositElemnt.innerText;
     const depositNumber = parseFloat(depositTotal);
     const depositUpdate = depositNumber + depositAmount ;
-    console.log('total Amunt', depositUpdate, typeof depositUpdate);
+    
+
+
     depositElemnt.innerText = depositUpdate;
     // Balance Update
     const balance = document.getElementById('balace-amount');
@@ -14,4 +17,42 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     const newBalance = depositAmount + balaceNumebr;
     balance.innerText = newBalance;
     depositAmountString.value = '';
+})
+
+
+// Withdraw Button curriculam
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const withdrawFlied = document.getElementById('withdraw-input-filed');
+    const withdrawFliedString = withdrawFlied.value ;
+    const withdrawAmount = parseFloat(withdrawFliedString);
+    
+    // Withdraw Amount
+    const withdraw = document.getElementById('withdraw-amount');
+    const withdrawString = withdraw.innerText;
+    const withdrawAmountNumber = parseFloat(withdrawString);
+
+
+
+    // Balance Calculation
+    const priveousBalance = document.getElementById('balace-amount');
+    const priveousBalanceString = priveousBalance.innerText;
+    const totalBalance = parseFloat(priveousBalanceString);
+    if(withdrawAmount <= totalBalance){
+        const newTotalBalance = totalBalance - withdrawAmount;
+
+    priveousBalance.innerText = newTotalBalance;
+    
+    // Calculation
+    const WithdrawTotal = withdrawAmountNumber + withdrawAmount;
+    withdraw.innerText = WithdrawTotal;
+    }
+    else{
+        alert('Eficensi Your Balance');
+    }
+    
+
+    
+
+    // Reset Input Filed
+    withdrawFlied.value = '';
 })
